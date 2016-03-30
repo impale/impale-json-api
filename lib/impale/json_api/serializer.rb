b3e36ab022317f4213ad912a40364fc9b397fb30
zippo <zippo@ezy.com>
2016-03-30 21:20:04 +0300

    add attributes

--- lib/impale/json_api/serializer.rb
+++ lib/impale/json_api/serializer.rb
@@ -1,10 +1,20 @@
 module Impale
   module JsonApi
     class Serializer
-      attr_reader :attributes, :obj
+      attr_reader :obj, :attributes
 
       def initialize(obj)
         @obj = obj
+        @attributes = self.class.attributes
+      end
+
+      class << self
+        def attributes(*_attributes)
+          unless _attributes.empty?
+            @attributes = _attributes
+          end
+          @attributes
+        end
       end
     end
   end
--- spec/impale/json_api/serializer_spec.rb
+++ spec/impale/json_api/serializer_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 require 'ostruct'
 
 serializer_class = Class.new(Impale::JsonApi::Serializer) do
-
+  attributes :name, :age,
+             password: :digest_password
 end
 
 describe Impale::JsonApi::Serializer do
-  let(:person) { OpenStruct.new(name: 'Pam', age: 24)}
+  let(:person) { OpenStruct.new(name: 'Pam', age: 24, digest_password: 'DWAaa')}
   subject(:serializer) { serializer_class.new(person) }
 
   describe 'initialize' do
@@ -16,7 +17,7 @@ describe Impale::JsonApi::Serializer do
   end
 
   describe 'attributes' do
-    xit 'has attributes' do
+    it 'has attributes' do
       expect(serializer.attributes).not_to be_nil
     end
   end
